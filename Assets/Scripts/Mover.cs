0b112a334c975253bcd07b7df7ec99e8d2571850
Alexander Warmouth <Alex@Alexanders-MBP.fios-router.home>
2019-06-02 18:23:35 -0400

    Initial Commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public string horizontalAxis;   // Input axis for horiz movement
+    public string verticalAxis;     // Input axis for vertical movement
+
+    public Worm worm;               // Reference to Worm component of WormBody game object
+    public GameObject otherEnd;
+    public bool isMoving;
+
+    private Mover otherMover;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        otherMover = otherEnd.GetComponent<Mover>();
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!worm.isDead)
+        {
+            Move();
+        }
+
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxisRaw(horizontalAxis);
+        float v = Input.GetAxisRaw(verticalAxis);
+        //Vector3 move = Vector3.ClampMagnitude(new Vector3(h, v), 1.0f);
+
+        if (!otherMover.isMoving && (Mathf.Abs(h) > 0 || Mathf.Abs(v) > 0))
+        {
+            Vector3 move = Vector3.ClampMagnitude(new Vector3(h, v), 1.0f);
+            isMoving = true;
+            anim.Play("Walk");
+            rb.mass = 1;
+            Vector3 targetPosition = transform.position + move * worm.speed * Time.deltaTime;
+            //if (Vector3.SqrMagnitude(targetPosition - otherEnd.transform.position) > worm.length * worm.length)
+            //{
+            //    targetPosition = otherEnd.transform.position + Vector3.ClampMagnitude(otherEnd.transform.position - targetPosition, worm.length);
+
+            //}
+            //transform.position = targetPosition;
+            rb.velocity = move * worm.speed;
+            //rb.MovePosition(targetPosition);
+        }
+        else
+        {
+            isMoving = false;
+            rb.velocity = Vector2.zero;
+            rb.mass = 100;
+            anim.Play("Idle");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Water") && !isMoving)
+        {
+            worm.Die();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        transform.rotation = worm.body.rotation;
+    }
+}
---
+++ Assets/Scripts/Worm.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Worm : MonoBehaviour
+{
+
+   public float maxLength;
+    public float speed;
+    public float health;
+    public Transform body;
+    public Transform head;
+    public Transform butt;
+    public Sprite[] bodySprites;
+    public bool isDead;
+
+    // Wormy UI
+    public Text note;
+    public Color noteColorGood;
+    public Color noteColorNeutral;
+    public Color noteColorBad;
+
+    [HideInInspector]
+    public float length;
+
+    //private LineRenderer line;
+    private Vector3 offset;
+    private SpriteRenderer bodySR;
+    private SpriteRenderer headSR;
+    private SpriteRenderer buttSR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = Camera.main.transform.position - body.position;
+        bodySR = body.gameObject.GetComponent<SpriteRenderer>();
+        headSR = head.gameObject.GetComponent<SpriteRenderer>();
+        buttSR = butt.gameObject.GetComponent<SpriteRenderer>();
+
+        note.color = noteColorGood;
+        note.text = "Let's Go!";
+        StartCoroutine(TextDelay());
+        //line = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        body.position = Vector3.Lerp(butt.position, head.position, 0.5f);
+        body.rotation = Quaternion.FromToRotation(Vector3.right, head.position - body.position);
+
+        length = Vector3.Magnitude(butt.position - head.position);
+        float bodyScale = Mathf.Clamp(length - 0.5f, 0.7f, 3.0f);
+        body.localScale = new Vector3(bodyScale, 1.0f, 1.0f);
+
+        if (length > 2.35f) bodySR.sprite = bodySprites[0];
+        else if (length > 2.2f) bodySR.sprite = bodySprites[1];
+        else if (length > 2.05f) bodySR.sprite = bodySprites[2];
+        else if (length > 1.9f) bodySR.sprite = bodySprites[3];
+        else if (length > 1.75f) bodySR.sprite = bodySprites[4];
+        else if (length > 1.5f) bodySR.sprite = bodySprites[5];
+        else bodySR.sprite = bodySprites[6];
+
+        if (butt.position.x > head.position.x)
+        {
+            bodySR.flipY = true;
+            headSR.flipY = true;
+            buttSR.flipY = true;
+        }
+        else
+        {
+            bodySR.flipY = false;
+            headSR.flipY = false;
+            buttSR.flipY = false;
+        }
+
+
+        //Debug.Log("Worm Length: " + length + " | Worm Sprite: " + bodySR.sprite.name);
+
+    }
+
+    public void Die()
+    {
+        isDead = true;
+        bodySR.color = noteColorBad;
+        headSR.color = noteColorBad;
+        buttSR.color = noteColorBad;
+        note.color = noteColorBad;
+        note.text = "You Have Drowned!";
+        StartCoroutine(Respawn());
+    }
+
+    private IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private IEnumerator TextDelay()
+    {
+        yield return new WaitForSeconds(3);
+        note.text = "";
+    }
+
+    void LateUpdate()
+    {
+        Camera.main.transform.position = body.position + offset;
+    }
+}
