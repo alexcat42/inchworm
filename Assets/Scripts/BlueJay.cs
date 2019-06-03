0dda26aecb4d6b54eec2271a90c5975a9b7129e3
Jeffu <jeffu@jeffu.tv>
2019-06-02 20:09:46 -0400

    Some updates

--- Assets/Scripts/BlueJay.cs
+++ Assets/Scripts/BlueJay.cs
@@ -2,17 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BluejayState
+{
+    patrolling,
+    attacking,
+    flying
+}
+
 public class BlueJay : MonoBehaviour
 {
+    public BluejayState state;
+    private GameObject worm;
+    private Worm wormScript;
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //worm = GameObject.FindWithTag("Player");
+        //wormScript = worm.GetComponent<Worm>();
+        anim = GetComponent<Animator>();
+        state = BluejayState.patrolling;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (state == BluejayState.patrolling)
+            Patrol();
+
+        else if (state == BluejayState.attacking)
+            Attack();
+
+        // Fly
     }
+
+    void Patrol()
+    {
+        anim.Play("Walk");
+    }
+
+    public void Attack()
+    {
+
+        anim.Play("Attack");
+    }
+
+
 }
